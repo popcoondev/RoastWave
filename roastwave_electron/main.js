353323385f950e6e3d1f0d732209ac1e00d2a292
popcoondev <popcoondev@gmail.com>
2024-02-17 00:50:57 +0900

    first commit

---
+++ roastwave_electron/main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html') // ここにFlutterビルドのWebアプリケーションのパスを指定
+}
+
+app.whenReady().then(createWindow)
