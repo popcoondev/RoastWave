2cab48a306e23592a60fe6504d5c7f1e8ebbc0f5
popcoondev <popcoondev@gmail.com>
2024-02-17 23:23:48 +0900

    .gitignoreとpackage.jsonに変更を加えました。シリアル通信を行うためのファイルを追加しました。

--- roastwave_electron/main.js
+++ roastwave_electron/main.js
@@ -1,4 +1,62 @@
 const { app, BrowserWindow } = require('electron')
+const { SerialCommunication } = require('./serial_communication')
+
+// コマンドライン引数からシリアルポートのパスを取得
+const portPath = process.argv[2];
+console.log('portPath:', portPath);
+
+// WebSocketサーバーを起動
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  console.log('A client connected');
+  
+  // クライアントからのメッセージ受信時に実行されるイベント
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+
+    // クライアントからのメッセージが「get_serial_port」の場合
+    // シリアルポートのリストを取得してクライアントに送信
+    if (message === 'get_serial_port') {
+      SerialCommunication.getSerialPortList().then(
+        ports => {
+          ports.forEach(port => {
+            console.log(port.path);
+            // シリアルポートリストをクライアントに送信
+            // 送信する際のフォーマットはJSON形式
+            ws.send(JSON.stringify({type: 'serial_port', data: port.path}));
+          });
+        },
+        err => {
+          console.error('Error listing ports', err);
+        }
+      );
+    }
+
+    // クライアントからのメッセージが「open_serial_port」の場合
+    // シリアルポートを開く
+    if (message === 'open_serial_port') {
+      const port = SerialCommunication.openSerialPort();
+      const parser = SerialCommunication.getSerialPortParser(port);
+      parser.on('data', (data) => {
+        console.log('Received:', data.toString());
+        // シリアルポートからのデータをWebSocketを通じてクライアントに送信
+        // 送信する際のフォーマットはJSON形式
+        ws.send(JSON.stringify({type: 'serial_data', data: data.toString()}));
+      });
+      port.on('error', function(err) {
+        console.log('Error: ', err.message)
+      });
+    }
+
+  });
+
+  // シリアルポートからのデータをWebSocketを通じてクライアントに送信
+  // ここでは例として「Hello Client」メッセージを送信しています
+  ws.send('Hello Client');
+});
+
 
 function createWindow () {
   const win = new BrowserWindow({
---
+++ roastwave_electron/serial_communication.js
@@ -0,0 +1,58 @@
+// main.js から呼び出される関数を定義するファイル
+
+// シリアル通信を行うためのファイル
+const { SerialPort } = require('serialport')
+const { ReadlineParser } = require('@serialport/parser-readline')
+
+// シリアルポートのパスをコマンドライン引数から取得
+const portPath = process.argv[2];
+
+// シリアルポートのリストを取得してmain.jsに返却する関数
+function getSerialPortList() {
+  return SerialPort.list();
+}
+
+// シリアルポートを開く関数
+function openSerialPort() {
+  return new SerialPort({path:portPath, baudRate: 115200});
+}
+
+// シリアルポートを開いて、データを受信できるparserを返却する関数
+// この関数はmain.jsから呼び出される
+function getSerialPortParser(port) {
+  return port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+}
+
+module.exports = {
+  getSerialPortList: getSerialPortList,
+  openSerialPort: openSerialPort,
+  getSerialPortParser: getSerialPortParser
+}
+
+// // シリアルポートを開く
+// //   ls /dev/tty.*   でポート名を確認
+// //   screen /dev/tty.usbmodem1101 115200 でシリアル通信を確認
+// const port = new SerialPort({path:portPath,
+//     baudRate: 115200,
+//    });
+ 
+//  // シリアルポートをスキャン
+//  SerialPort.list().then(
+//    ports => {
+//      ports.forEach(port => {
+//        console.log(port.path);
+//      });
+//    },
+//    err => {
+//      console.error('Error listing ports', err);
+//    }
+//  );
+ 
+//  const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+//  parser.on('data', (data) => {
+//        console.log('Received:', data.toString());
+//  });
+ 
+//  port.on('error', function(err) {
+//    console.log('Error: ', err.message)
+//  });
